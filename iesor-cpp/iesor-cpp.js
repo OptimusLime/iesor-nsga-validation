2b1c92933a17f65de30ec6f45ba7159c51f2b0a9
Paul Szerlip <paul.szerlip@knights.ucf.edu>
2014-04-07 13:21:34 -0400

    Successful parent publishing! Forgot to load seeds into evolution. Now everything is working hunky doory.
    Next step: Draw these goofy guys properly. Also need a home page for starting new evolution rounds.
    
    Then build phylogeny and finalize images.

--- iesor-cpp/iesor-cpp.js
+++ iesor-cpp/iesor-cpp.js
@@ -1,6 +1,8 @@
 //here we test the insert functions
 //making sure the database is filled with objects of the schema type
 
+
+var Q = require('q');
 var iesor = require('node-iesor');
 var winneat = require('win-neat');
 var cppnjs = require('cppnjs');
@@ -72,6 +74,9 @@ function iesorcpp(backbone, globalConfig, localConfig)
 	self.log.logLevel = localConfig.logLevel || self.log.normal;
 
 	self.iesorPoolSize = localConfig.iesorPoolSize || 10;
+
+	//make sure to have enough world for async simulation
+	self.iesorPoolSize = Math.max(self.iesorPoolSize, 2*globalConfig.searchParameters.populationSize || 10);
 	// self.iesorReserveSize = localConfig.iesorReserveSize || 4;
 
 
@@ -143,6 +148,123 @@ function iesorcpp(backbone, globalConfig, localConfig)
 
 	}
 
+	self.promiseSimulation = function(tArtifact, baseEval, iWorld, options)
+	{
+		var defer = Q.defer();
+
+		//3.5 second simulation
+		var simTimeMS = options.simulationTimeMS || 3500;
+		var efficiency = !(options.searchRawDistance || false);
+
+		//how many frames of animation to preview
+		var frameCount = options.framesToSave || 7;
+
+		//fill in with eveyrthing else -- process.nexttick is too powerful
+		setTimeout(function()
+		// process.nextTick(function()
+		{
+			//this is the true object -- we need to convert into the world 
+			var genome = tArtifact.genome;
+
+			//now we're ready
+			var byteNetwork = neatGenomeToByteCode(genome);
+
+			//for now, we transfer knowledge via string to the C++ code 
+			//some performance penalty for this -- probably not as bad as simulating the whole thing in javascript
+			var byteString = JSON.stringify(byteNetwork);
+
+			//now we load the body first
+			var bodyString = iWorld.convertNetworkToBody(byteString);
+
+			//if we don't get anything back -- errors all over the place
+			bodyJSON = JSON.parse(bodyString);
+
+			//now we check if the body even exists -- if it doesn't don't bother simulation
+			if(!bodyJSON.nodes.length)
+			{
+				//this eval was a bust -- it's empty
+				// emptyBodyCount++;
+
+				//all done!
+				defer.resolve(true);
+				return;
+			}
+
+			//pass the body into the eval for future use -- this gun by fun
+			// baseEval.iesorBody = bodyString;
+			baseEval.iesorFrames = [];
+
+
+			//string with our morph info inside
+			var morphString = iWorld.loadBodyFromNetwork(byteString);
+
+			//get our morphology info						
+			var morphology = JSON.parse(morphString);
+
+			//we can optionally include mass as part of our fitness metric
+			//distance/mass == efficiency of muscles
+			var mass = parseFloat(morphology.mass);
+
+			//set morphology behavior
+			setMorphologyBehavior(morphology, baseEval);
+
+			// self.log("Generated morphology: ", morphology);
+
+			var com = {x: parseFloat(morphology.comX), y: parseFloat(morphology.comY)};
+
+			//grab initial drawing
+			//pull the drawing info -- just a single frame worth
+			baseEval.iesorFrames.push(iWorld.getWorldDrawList());
+
+			//now the remainder of the frames are captured after the initial
+			if(frameCount -1 > 0)
+			{
+				var simtime = simTimeMS/(frameCount-1);
+				for(var f=0; f < frameCount-1; f++)
+				{
+					//simulate for the simtime/frame count
+					iWorld.simulateWorldMS(simtime);
+
+					//pull the drawing info -- just a single frame worth -- as many frames as requested -- in string form
+					baseEval.iesorFrames.push(iWorld.getWorldDrawList());
+				}
+			}
+		
+			//now we need to pull out distance traveled for the object
+
+			//simulate for 3.5 seconds by default
+			//tell the wrold to simulate
+			var simInfo = iWorld.simulateWorldMS(simTimeMS);
+
+			// self.log('Sim info: ', JSON.parse(simInfo));
+
+			//now we reach into the world and get some other info
+			var postCom = iWorld.bodyCenterOfMass();
+
+			//now grab the com after the simulation
+			postCom = JSON.parse(postCom);
+
+			//calculate distance
+			var totalDist = Math.abs(postCom.x - com.x);
+
+			// self.log("Xeffic: ", totalDist/mass ," -- Pre com: ", com, " post com: ", postCom);
+
+			//then finally set distance/mass -- for efficiency
+			baseEval.realFitness = efficiency ? totalDist/mass : totalDist;
+			baseEval.fitness = efficiency ? totalDist/mass : totalDist; 
+			baseEval.iesorDistance = totalDist;
+
+
+			//all done
+			defer.resolve(false);
+
+		// });
+		},Math.floor(Math.random()*150));
+
+
+		return defer.promise;
+	}
+
 	self.eventCallbacks = function()
 	{ 
 		return {
@@ -160,9 +282,7 @@ function iesorcpp(backbone, globalConfig, localConfig)
 
 				//3.5 second simulation
 				var simTimeMS = options.simulationTimeMS || 3500;
-
 				var efficiency = !(options.searchRawDistance || false);
-
 				var framesToSave = options.framesToSave || 7;
 
 				//evals we will return by indexed artifacts
@@ -176,119 +296,63 @@ function iesorcpp(backbone, globalConfig, localConfig)
 
 				try
 				{
+
+					var allPromises = [];
+
 					//let's do an eval -- write now this is synchronous
 					for(var index in indexArtifacts)
 					{
+						//clone options for the single simulation objects
+						var optionsClone = JSON.parse(JSON.stringify(options));
+
 						//inc our count
 						allCount++;
 
 						var tArtifact = indexArtifacts[index];
 
-						//this is the true object -- we need to convert into the world 
-						var genome = tArtifact.genome;
-
 						//default eval
-						var baseEval = {realFitness: 0.0000001, behaviors : defaultBehavior(), complexity: genome.connections.length + genome.nodes.length, iesorDistance: 0};
+						var baseEval = {
+							realFitness: 0.0000001, 
+							behaviors : defaultBehavior(), 
+							complexity: tArtifact.genome.connections.length + tArtifact.genome.nodes.length, 
+							iesorDistance: 0};
 
 						//evaluation for the index
 						evals[index] = baseEval;
 
-						//now we're ready
-						var byteNetwork = neatGenomeToByteCode(genome);
-
-						//for now, we transfer knowledge via string to the C++ code 
-						//some performance penalty for this -- probably not as bad as simulating the whole thing in javascript
-						var byteString = JSON.stringify(byteNetwork);
-
 						//now what yo?
 						var iWorld = getNextWorld(); // this will clean everything up for us!
 
-						//now we load the body first
-						var bodyString = iWorld.convertNetworkToBody(byteString);
-
-						//if we don't get anything back -- errors all over the place
-						bodyJSON = JSON.parse(bodyString);
-
-						//now we check if the body even exists -- if it doesn't don't bother simulation
-						if(!bodyJSON.nodes.length)
-						{
-							//this eval was a bust -- it's empty
-							emptyBodyCount++;
-							continue;
-						}
-
-						//pass the body into the eval for future use -- this gun by fun
-						// baseEval.iesorBody = bodyString;
-						baseEval.iesorFrames = [];
-
-						//how many frames of animation to preview
-						var frameCount = 7;
-
-						//string with our morph info inside
-						var morphString = iWorld.loadBodyFromNetwork(byteString);
-
-						//get our morphology info						
-						var morphology = JSON.parse(morphString);
-
-						//we can optionally include mass as part of our fitness metric
-						//distance/mass == efficiency of muscles
-						var mass = parseFloat(morphology.mass);
-
-						//set morphology behavior
-						setMorphologyBehavior(morphology, baseEval);
-
-						// self.log("Generated morphology: ", morphology);
-
-						var com = {x: parseFloat(morphology.comX), y: parseFloat(morphology.comY)};
-
-						//grab initial drawing
-						//pull the drawing info -- just a single frame worth
-						baseEval.iesorFrames.push(iWorld.getWorldDrawList());
-
-						//now the remainder of the frames are captured after the initial
-						if(frameCount -1 > 0)
-						{
-							var simtime = simTimeMS/(frameCount-1);
-							for(var f=0; f < frameCount-1; f++)
-							{
-								//simulate for the simtime/frame count
-								iWorld.simulateWorldMS(simtime);
-
-								//pull the drawing info -- just a single frame worth -- as many frames as requested -- in string form
-								baseEval.iesorFrames.push(iWorld.getWorldDrawList());
-							}
-						}
-					
-						//now we need to pull out distance traveled for the object
-
-						//simulate for 3.5 seconds by default
-						//tell the wrold to simulate
-						var simInfo = iWorld.simulateWorldMS(simTimeMS);
-
-						// self.log('Sim info: ', JSON.parse(simInfo));
-
-						//now we reach into the world and get some other info
-						var postCom = iWorld.bodyCenterOfMass();
-
-						//now grab the com after the simulation
-						postCom = JSON.parse(postCom);
-
-
-						//calculate distance
-						var totalDist = Math.abs(postCom.x - com.x);
-
-						// self.log("Xeffic: ", totalDist/mass ," -- Pre com: ", com, " post com: ", postCom);
-
-						//then finally set distance/mass -- for efficiency
-						baseEval.realFitness = efficiency ? totalDist/mass : totalDist;
-						baseEval.fitness = efficiency ? totalDist/mass : totalDist; 
-						baseEval.iesorDistance = totalDist;
-
+						allPromises.push(self.promiseSimulation(tArtifact, baseEval, iWorld, optionsClone));
 					}
+					
+					// self.log("Empty bodies ", emptyBodyCount, " out of ", allCount, "-- %: ", emptyBodyCount/allCount);
 
-					self.log("Empty bodies ", emptyBodyCount, " out of ", allCount, "-- %: ", emptyBodyCount/allCount);
+					Q.all(allPromises)
+						.then(function(promises)
+						{
+							var emptyBodyCount = 0;
 
-					finished(undefined, evals);
+							//wa wa weee waaa we got some promises -- that tell us if we were empty or not
+							for(var i=0; i < promises.length; i++)
+							{
+								if(promises[i])
+									emptyBodyCount++;
+
+							}
+
+							//how many were empty?
+							self.log("Empty bodies ", emptyBodyCount, " out of ", allCount, "-- %: ", emptyBodyCount/allCount);
+
+							//all setup -- finished!
+							finished(undefined, evals);
+						})
+						.fail(function(err)
+						{
+							//somethign failed -- let it be known
+							finished(err);
+						})
+					// finished(undefined, evals);
 
 				}
 				catch(e)
